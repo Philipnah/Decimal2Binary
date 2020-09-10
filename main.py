1277c70d8d2fc1914540c83993889f47c42a6a93
Philipnah <Philip@han.dk>
2020-09-10 09:01:47 +0200

    Added choice system

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
 
-number = int(input("\nDecimal number: "))
+choice = input("Please choose a conversion:\n1: Decimal to binary\n2: Decimal to hexadecimal\n3: Binary to decimal\n4: Hexadecimal to decimal\n5: Binary to hexadecimal\n6: Hexadecimal to binary\n\n: ")
 
-binary = []
 
-while (number > 0):
-	binary.append(number % 2)
-	number = number//2
-	
+if choice == "1":
+	d2b()
+elif choice == "2":
+	d2h()
+elif choice == "3":
+	b2d()
+elif choice == "4":
+	h2d()
+elif choice == "5":
+	b2h()
+elif choice == "6":
+	h2b()
+else:
+	print("Your selection was not in the register. Please choose another option.")
 
-# [::-1] is used to reverse the array
-print("\nBinary:" + "\n" + str(binary[::-1]))
 
-for i in binary:
-	print(i, end="")
-print("\n")
+def d2b():
+	number = int(input("\nDecimal number: "))
+
+	binary = []
+
+	while (number > 0):
+		binary.append(number % 2)
+		number = number//2
+		
+
+	# [::-1] is used to reverse the array
+	print("\nBinary:" + "\n" + str(binary[::-1]))
+
+	for i in binary:
+		print(i, end="")
+	print("\n")
+
+def d2h():
+	pass
+
+def b2d():
+	pass
+
+def h2d():
+	pass
+
+def b2h():
+	pass
+
+def h2b():
+	pass
